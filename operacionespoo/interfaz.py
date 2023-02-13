b86e88e17055a3daf23d8a1405b1f4a173d753fa
Carrotw <91509157+MetTheCarrot@users.noreply.github.com>
2023-02-12 21:26:03 -0500

    Refactor: Primer código
    Cambie la funcionalidad de la interfaz a mi forma de realizarlo.

--- operacionespoo/interfaz.py
+++ operacionespoo/interfaz.py
@@ -2,68 +2,93 @@ from operaciones import operaciones
 
 class interfaz:
     def __init__(self):
-        self.opr = None
-    
+        self.operaciones = None
+
     def menu(self):
-        salir = False
-        opc = 0
-        while salir is False:
-            print("Bienvenidos a mi programa")
-            print("##########################")
-            print("Seleccione una de las siguientes opciones")
-            print("1. Ingresar los valores.")
-            print("2. Sumar valores.")
-            print("3. Restar valores.")
-            print("4. Multiplicar valores.")
-            print("5. Dividir valores.")
-            print("##########################")
-            print("0. Para salir del programa :'(.")
-            opc = input()
-            
-            try:
-                opc = int(opc)
-            except:
-                opc = 6
-            if opc > 5:
-                print("Valor de opcion  no valida")
-            elif opc == 0:
-                salir = True
-            else:
-                self.opciones(opc)
-                
-                
+        self.clearOS()
+        print("""
+        Bienvenidos a mi programa
+        #########################
+        Seleccione una opción:
+        [1] Ingresar números
+        [2] Sumar
+        [3] Restar
+        [4] Multiplicar
+        [5] Dividir
+        #########################
+        [0] Salir
+        """)
+        opc = self.intInput("Ingrese una opción: ")
+        self.opciones(opc)
     
-    def opciones(self, opc):
+    def checkOperaciones(self, opc : int):
+        """_summary_ : Función para validar que se hayan ingresado los números"""
+        if(opc == 1): # Opción para ingresar los números
+            return 
+        if self.operaciones == None:
+            print("Debe ingresar los números primero")
+            self.pauseOS()
+            self.menu()
+            return
+
+    def opciones(self, opc : int):
+        """_summary_ : Función para validar las opciones del menú"""
+        self.clearOS()
+        if(opc == 0):
+            print("Gracias por utilizar mi programa")
+            return # Salir del programa
+        self.checkOperaciones()
         match opc:
             case 1:
-                print("Por favor escriba el valor de a")
-                a = int(input())
-                print("Por favor escriba el valor de b")
-                b = int(input())
-                self.opr = operaciones(a, b)
+                self.operaciones = operaciones(
+                    self.intInput("Ingrese el primer número: "), 
+                    self.intInput("Ingrese el segundo número: ")
+                    )
             case 2:
-                if self.opr is None:
-                    print("Pör favor ingrese los valores primero")
-                else:
-                    print("El resultado de la suma es:")
-                    print(self.opr.sumar())
+                print(f'La suma es: {self.operaciones.sumar()}')
             case 3:
-                if self.opr is None:
-                    print("Pör favor ingrese los valores primero")    
-                else:
-                    print("El resultado de la resta es:")
-                    print(self.opr.restar())
+                print(f'La resta es: {self.operaciones.restar()}')
             case 4:
-                if self.opr is None:
-                    print("Pör favor ingrese los valores primero")    
-                else:
-                    print("El resultado de la multiplicacion es:")
-                    print(self.opr.multiplicar())
+                print(f'La multiplicación es: {self.operaciones.multiplicar()}')
             case 5:
-                if self.opr is None:
-                    print("Pör favor ingrese los valores primero")    
-                else:    
-                    print("El resultado de la division es:")
-                    print(self.opr.dividir())
-                
-        pass
+                print(f'La división es: {self.operaciones.dividir()}')
+            case _:
+                print("Opción incorrecta")
+        self.pauseOS()      
+        self.menu()
+    
+    def intInput(self, msg : str):
+        """_summary_ : Función para validar que el usuario ingrese un número entero
+
+        Args:
+            msg (str): _Descripción del mensaje de solicitud_
+
+        Returns:
+            _int_: _retorna un numero tipo int, no podra ser otro_
+        """
+        while True:
+            try:
+                x = int(input(msg))
+                return x
+            except ValueError:
+                print("Error: Debe ingresar un número entero.")    
+    
+    def clearOS(self):
+        """_summary_ : Función para limpiar la pantalla
+        """
+        from os import system as cmd
+        from platform import system as os
+        if os() == "Windows":
+            cmd("cls")
+        else:
+            cmd("clear")
+
+    def pauseOS(self):
+        """_summary_ : Función para pausar la ejecución del programa
+        """
+        from os import system as cmd
+        from platform import system as os
+        if os() == "Windows":
+            cmd("pause")
+        else:
+            cmd("read -p 'Presione una tecla para continuar...' var")
